e4929c679cafba0bd4f1a0bbfbb55f591437d61e
Ramiro Menendez <ramiro99@live.com.ar>
2023-10-05 14:58:51 -0300

    r

--- 2023/code.cpp
+++ 2023/code.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <iomanip>
-#define pi 3.14159
+#include <stdlib.h>
 
 using namespace std;
 
 int main() {
 
-    double A, B, C;
+    int a, b, c, greatest;
 
-    cin >> A >> B >> C;
+    cin >> a >> b >> c;
 
-    cout << "TRIANGULO: " << setprecision(3) << fixed
-        << A*C/2 << endl
-        << "CIRCULO: " << setprecision(3) << fixed
-        << pi*C*C << endl
-        << "TRAPEZIO: " << setprecision(3) << fixed
-        << (A*C+B*C)/2 << endl
-        << "QUADRADO: " << setprecision(3) << fixed
-        << B*B << endl
-        << "RETANGULO: " << setprecision(3) << fixed
-        << A*B << endl;
+    greatest = (a+b+abs(a-b))/2;
+    greatest = (greatest+c+abs(greatest-c))/2;
+
+    cout << greatest << " eh o maior" << endl;
+
+    system("pause");
 }
